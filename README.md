2928001318893cdb1dad8f432ee09cea92678e73
Franco Lombardo <franco.lombardo@a-cube.io>
2024-07-10 19:11:19 +0200

    Using @misskey-dev/node-http-message-signatures

--- README.md
+++ README.md
@@ -3,14 +3,15 @@
 This project is a sample implementation of a webhook that [checks the validity of A-Cube signature](https://docs.acubeapi.com/documentation/common/http-signature/#python).
 
 To run it, first install dependencies with
-
 ```shell
 npm install
 ```
-
+Compile Typescript code with
+```shell
+npx tsc
+```
 Then start the server with
 ```shell
 npm start
 ```
-
 The server should now be running on port 3000.
---
+++ src/index.ts
@@ -0,0 +1,41 @@
+// src/index.ts
+import express, {Request, Response} from 'express';
+
+import axios from 'axios';
+import {
+  parseDraftRequest,
+  verifyDraftSignature
+} from "@misskey-dev/node-http-message-signatures";
+
+async function getKeyId(): Promise<string> {
+  const response = await axios.get('https://common-sandbox.api.acubeapi.com/signature-public-key');
+  return response.data.public_key;
+}
+
+async function verifyHttpSignature(request: Request): Promise<boolean> {
+  const publicKey = await getKeyId(); // Ideally, cache or retrieve more efficiently
+  try {
+    let parsedSignature = parseDraftRequest(request);
+
+    return await verifyDraftSignature(parsedSignature['value'], publicKey, (...args) => console.log(args));
+  } catch (error) {
+    console.error(error);
+    return false;
+  }
+}
+
+const app = express();
+
+app.post('/my_webhook', async (req: Request, res: Response) => {
+  if (!await verifyHttpSignature(req)) {
+    return res.status(401).send({message: 'Invalid http signature'});
+  }
+
+  res.send({message: 'Done'});
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on ${PORT}`);
+});
+
